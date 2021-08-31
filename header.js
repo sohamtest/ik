b8a9c7a84f46a09fba672f3f805311d0514aa21f
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2021-08-31 11:43:34 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,33 @@
+<link href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.3/css/intlTelInput.min.css" rel="stylesheet"/>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+<meta content='849281225124904' property='fb:pages'>
+<meta name="theme-color" content="#3996d2"/>
+<meta name="google-site-verification" content="wUJdF2phjHl5IfpFMXn-BXoaPku3ofIKiAKWRg3LhYc" />
+
+<style>
+  .iti--allow-dropdown{grid-column-end: span 2;margin-bottom:10px;width:100%;}
+</style>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P335R9N');</script>
+<!-- End Google Tag Manager -->
+
+
+<!-- Anti-flicker snippet (recommended)  -->
+<style>.async-hide { opacity: 0 !important} </style>
+<script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+})(window,document.documentElement,'async-hide','dataLayer',4000,
+{'GTM-TDQF9JQ':true});</script>
+
+<script defer src="https://www.googleoptimize.com/optimize.js?id=GTM-TDQF9JQ"></script>
+
+
+<!-- Calendly link widget begin -->
+<script defer src="https://calendly.com/assets/external/widget.js" type="text/javascript" async></script>
+<!-- Calendly link widget end -->
