4e6189c789fc4636fe9cbb89d6cd6d541d297ad5
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2022-12-05 13:22:38 +0530

    Update and rename byecalendly-v1.9.js to byecalendly-v2.js

--- byecalendly-v2.js
+++ byecalendly-v2.js
@@ -7,7 +7,7 @@ var int_phone3;
   
  const t_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   
- let api_url = "https://uplevel.interviewkickstart.com/api/webinar-slot/upcoming-slots/?country=IND&program=Backend&timezone="+v_timezone;
+ let api_url = "https://uplevel.interviewkickstart.com/api/webinar-slot/upcoming-slots/?country=USA&program=Backend&timezone="+v_timezone;
 	
   let xhr = new XMLHttpRequest();
     xhr.open('GET',api_url,true);
@@ -143,7 +143,7 @@ var int_phone3;
             $('.webinar__registration-form2').submit();
             setTimeout(function(){
               location.href = finalurl;
-            }, 1000);
+            }, 800);
         }
      });
 });
