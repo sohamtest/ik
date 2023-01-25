49b36b6548c7fb82a6695252e9856862a5782b52
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2023-01-25 12:51:18 +0530

    Update and rename exit-intent-bc-popup-v1.4.js to exit-intent-bc-popup-v1.5.js

--- exit-intent-bc-popup-v1.5.js
+++ exit-intent-bc-popup-v1.5.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     exitIntentBCpopup = (exitIntentBCpopup == '') ? true : exitIntentBCpopup;
     setCookie("exitIntentBCpopup",false);
 
-    if(exitIntentBCpopup){
+    
         // scrolling
         let prevScrollpos = window.pageYOffset;
         window.addEventListener("scroll", function () {
@@ -11,22 +11,23 @@ $(document).ready(function () {
 
             // Check if the scroll position is 1 rem or less
             //console.log(currentScrollPos, prevScrollpos)
+            if(exitIntentBCpopup){
+                if (prevScrollpos > currentScrollPos && (prevScrollpos - currentScrollPos) >= 400) {
+                    $('.webinar__lightbox').css('display','flex');
+                    dataLayer.push({
+                        'event': 'exit_intent_bye_calendly_popup',
+                        'eventCategory': 'exit_intent_bye_calendly_popup',
+                        'eventAction': 'exit_intent_bye_calendly_popup',
+                        'eventLabel': 'Form triggered on scroll'
+                    });
+                    exitIntentBCpopup = false;
+                }
 
-            if (prevScrollpos > currentScrollPos && (prevScrollpos - currentScrollPos) >= 400) {
-                $('.webinar__lightbox').css('display','flex');
-                dataLayer.push({
-                    'event': 'exit_intent_bye_calendly_popup',
-                    'eventCategory': 'exit_intent_bye_calendly_popup',
-                    'eventAction': 'exit_intent_bye_calendly_popup',
-                    'eventLabel': 'Form triggered on scroll'
-                });
-                exitIntentBCpopup = false;
+                if (currentScrollPos > prevScrollpos)
+                prevScrollpos = currentScrollPos
             }
-
-            if (currentScrollPos > prevScrollpos)
-            prevScrollpos = currentScrollPos
         });
-      }
+ 
 
     $(document).mouseleave(function () {
         if(exitIntentBCpopup){
