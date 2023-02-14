a055cf1b34127ccca87edc4037a4d01a38162a6b
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2023-02-14 15:41:22 +0530

    Create exit-intent-bc-popup-v1.2.js

---
+++ exit-intent-bc-popup-v1.2.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    let exitIntentBCpopup = getCookie("exitIntentBCpopup");
+    
+    if((exitIntentBCpopup != "false") && (bcExitIntentOnBrowserTab == "true")){
+        setCookie("exitIntentBCpopup","false");
+        exitIntentBCpopup = true;
+    }else{
+        exitIntentBCpopup = false;
+    }
+    
+        // scrolling
+        let prevScrollpos = window.pageYOffset;
+        window.addEventListener("scroll", function () {
+            let currentScrollPos = window.pageYOffset;
+
+            // Check if the scroll position is 1 rem or less
+            //console.log(currentScrollPos, prevScrollpos)
+            if(exitIntentBCpopup){
+                if (prevScrollpos > currentScrollPos && (prevScrollpos - currentScrollPos) >= 400) {
+                    $('.webinar__lightbox').css('display','flex');
+                    dataLayer.push({
+                        'event': 'exit_intent_bye_calendly_popup',
+                        'eventCategory': 'exit_intent_bye_calendly_popup',
+                        'eventAction': 'exit_intent_bye_calendly_popup',
+                        'eventLabel': 'Form triggered on scroll'
+                    });
+                    exitIntentBCpopup = false;
+                }
+
+                if (currentScrollPos > prevScrollpos)
+                prevScrollpos = currentScrollPos
+            }
+        });
+ 
+
+    $(document).mouseleave(function () {
+        if(exitIntentBCpopup){
+            $('.webinar__lightbox').css('display','flex');
+            dataLayer.push({
+                'event': 'exit_intent_bye_calendly_popup',
+                'eventCategory': 'exit_intent_bye_calendly_popup',
+                'eventAction': 'exit_intent_bye_calendly_popup',
+                'eventLabel': 'Form triggered on exit intent'
+            });
+            exitIntentBCpopup = false;
+        }
+    });
+});
