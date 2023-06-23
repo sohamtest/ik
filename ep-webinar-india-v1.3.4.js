1b808a7f08b50edaab4d484c521a3c938af0a117
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2023-06-23 20:12:39 +0530

    Update and rename ep-webinar-india-v1.3.3.js to ep-webinar-india-v1.3.4.js

--- ep-webinar-india-v1.3.4.js
+++ ep-webinar-india-v1.3.4.js
@@ -79,7 +79,7 @@ function populateWebinarSlots(resobj){
         
         for (i=0;i<nslots;i++){
           var hdate = resobj[i].weekday +", "+ resobj[i].day + " " + t_months[parseInt(resobj[i].month)-1] + " "+ resobj[i].year + " | " + resobj[i].hour +":" + resobj[i].minute +" "+resobj[i].am_or_pm;
-					var radiohtml = '<label class="select-webinar-slot w-radio"><input type="radio" name="start-date" value="'+ resobj[i].start_time +'" data-endtime="'+resobj[i].end_time+'" data-invitee_starttime="'+ resobj[i].invitee_start_time +'" data-invitee_endtime="'+ resobj[i].invitee_end_time +'" data-name="'+ resobj[i].start_time +'" class="w-form-formradioinput select-webinar-radio-btn w-radio-input" data-webinar_lead_type="'+ resobj[i].webinar_lead_type +'"><span class="w-form-label" for="start-date-'+i+'">'+hdate+' '+ tz +'</span></label>';
+	  var radiohtml = '<label class="select-webinar-slot w-radio"><input type="radio" name="start-date" value="'+ resobj[i].start_time +'" data-endtime="'+resobj[i].end_time+'" data-invitee_starttime="'+ resobj[i].invitee_start_time +'" data-invitee_endtime="'+ resobj[i].invitee_end_time +'" data-name="'+ resobj[i].start_time +'" data-webinar_lead_type="'+ resobj[i].webinar_lead_type +'" class="w-form-formradioinput select-webinar-radio-btn w-radio-input"><span class="w-form-label" for="start-date-'+i+'">'+hdate+' '+ tz +'</span></label>';
           $('.webinar__slots').append(
             $(radiohtml)
           );
